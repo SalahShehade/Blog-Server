0935aa768419aeb6a057700c1185f95b05de5d10
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2024-12-11 19:11:34 +0200

    n

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ server.listen(process.env.PORT || 5000, () => {
 });
 
 //
-
+app.listen(5000, "0.0.0.0", () => {
+  console.log("Server is running on port 5000");
+});
 const events = require("events");
 events.EventEmitter.defaultMaxListeners = 15; // Increase the limit to 15 or more as needed
 
