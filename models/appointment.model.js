d4c4dd7d66b9b6641b5309b3fca4fbf06b35de45
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2024-12-11 18:30:32 +0200

    added status flag

--- models/appointment.model.js
+++ models/appointment.model.js
@@ -2,10 +2,15 @@ const mongoose = require("mongoose");
 
 const appointmentSchema = new mongoose.Schema({
   blogId: { type: String, required: true },
-  userName: { type: String, required: true },
-  time: { type: String, required: true },
-  duration: { type: Number, default: 30 },
+  userName: { type: String, required: true, default: "Available Slot" }, // Default for available slots
+  time: { type: String, required: true }, // Time in HH:mm format
+  duration: { type: Number, default: 30 }, // Default duration 30 minutes
   isConfirmed: { type: Boolean, default: false },
+  status: {
+    type: String,
+    enum: ["booked", "available"],
+    default: "available", // Available slots are "available" by default
+  },
 });
 
 module.exports = mongoose.model("Appointment", appointmentSchema);
