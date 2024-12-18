ad617fc3640273ea6feb24c6107bbb863bcbbc5b
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2024-12-18 18:22:11 +0200

    nn

--- routes/blogpost.js
+++ routes/blogpost.js
@@ -31,7 +31,7 @@ const upload = multer({
 router.route("/Add").post(middleware.checkToken, async (req, res) => {
   const blogpost = BlogPost({
     email: req.decoded.email,
-    username: req.decoded.username,
+    username: req.body.username,
     title: req.body.title,
     body: req.body.body,
     type: req.body.type,
