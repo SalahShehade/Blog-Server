5ae66c42240e7eb67b13744c3832067a2280688d
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2024-12-20 16:53:40 +0200

    removed cloud

--- cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: "hosmqlbgc",
-  api_key: "935839451824915",
-  api_secret: "GHefu5NCzdM9Cb2h8aVWw5vh6eM",
-});
-
-module.exports = cloudinary;
