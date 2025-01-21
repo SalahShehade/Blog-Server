b9d5805d26be93c3f0a87d4828ea8ab73206ef2a
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2025-01-21 11:35:23 +0200

    added date column

--- models/appointment.model.js
+++ models/appointment.model.js
@@ -4,6 +4,7 @@ const appointmentSchema = new mongoose.Schema({
   blogId: { type: String, required: true },
   userName: { type: String, required: true, default: "Available Slot" }, // Default for available slots
   time: { type: String, required: true }, // Time in HH:mm format
+  date: { type: String }, // e.g. "2025-01-12"
   duration: { type: Number, default: 30 }, // Default duration 30 minutes
   isConfirmed: { type: Boolean, default: false },
   status: {
